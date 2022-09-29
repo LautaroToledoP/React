88b97ae2b9ad56524008edde5a66d9b416c39830
Lautaro Toledo <lautaroestevez17@gmail.com>
2022-09-29 05:03:19 -0300

    react

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>Manga-Store</h1>
+            <div>
+                <button>Mangas Disponibles</button>
+            </div>
+            <div>
+                <button>Mangas Populares</button>
+            </div>
+        </nav>
+
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+export default Navbar
