1e512dd3ef1afb8d1271c96a9521b1c3a88d78ea
Lautaro Toledo <lautaroestevez17@gmail.com>
2022-11-05 18:22:09 -0300

    firebase2

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,40 +1,50 @@
 import { useState, useEffect } from 'react'
-// import { getProductById, getProducts } from '../../asyncMock'
 import ItemListDetail from '../ItemListDetail/ItemListDetail'
-import { useParams, useNavigate } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { getDoc, doc, collection } from 'firebase/firestore'
 import { reference } from '../../services/firebase'
 
 
 const ItemDetailContainer = ({setCart}) => {
     const [product, setProduct] = useState([])
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(true)
 
 
-    const { id } = useParams()
+    const { productId } = useParams()
     
 
     useEffect(() => {
-        setLoading(true)
 
-       const ref = reference
-       const productCollection = ref.collection('1')
-       const collectionToShow = productCollection.doc(id)
+        const docRef = doc(reference, 'products', productId)
+
+        getDoc(docRef).then(response => {
+            const data = response.data()
+            const productAdapted = {id: response.id, ...data}
+            setProduct(productAdapted)
+        }).finally(() => {
+            setLoading(false)
+        })
+    }, [productId])
+//         setLoading(true)
+
+//        const ref = reference
+//        const productCollection = ref.collection('1')
+//        const collectionToShow = productCollection.doc(productId)
 
 
-       collectionToShow.get().then((doc) => {
-        if (!doc.exists) {
-            console.log('product does not exist!')
-            return;
-        }
-        console.log('product found!')
-        setProduct({id: doc.id, ...doc.data() })
-        console.log(`product ${product.id}`)
-       }).catch((error) => {console.log('error searching products', error)
-    }).finally(() => {
-        setLoading(false)
-    })
-  }, [id])
+//        collectionToShow.get().then((doc) => {
+//         if (!doc.exists) {
+//             console.log('product does not exist!')
+//             return;
+//         }
+//         console.log('product found!')
+//         setProduct({id: doc.id, ...doc.data() })
+//         console.log(`product ${product.id}`)
+//        }).catch((error) => {console.log('error searching products', error)
+//     }).finally(() => {
+//         setLoading(false)
+//     })
+//   }, [productId])
   
     if(loading) {
         return <h1>Cargando detalle...</h1>
